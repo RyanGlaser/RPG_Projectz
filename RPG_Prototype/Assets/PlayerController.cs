c753048da543860039fad098bfa66a912fe56b50
RyoTheMagicMan <ryanglaser150@gmail.com>
2018-06-28 18:07:46 -0500

    Inital Commit

---
+++ RPG_Prototype/Assets/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour {
+
+    public LayerMask movementMask;
+    Camera cam;
+    PlayerMotor motor;
+
+
+	// Use this for initialization
+	void Start () {
+        cam = Camera.main;
+        motor = GetComponent<PlayerMotor>();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))    // check for left click
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100, movementMask))
+            {
+                motor.MoveToPoint(hit.point);
+                // move our player to the location they click
+
+                // stop focusing any objects 
+            }
+        }
+
+        if (Input.GetMouseButtonDown(1))    // check for right click
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                // check if we hit an interactable if we did set it as our focus
+            }
+        }
+    }
+}
